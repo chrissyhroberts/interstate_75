f94bbeae2aba092d597b202121a76864f9945873
chrissyhroberts <chrissyhroberts@yahoo.co.uk>
2023-10-24 18:35:27 +0100

    ADDS scrolling marquee functions

---
+++ main.py
@@ -0,0 +1,5 @@
+# boot.py
+
+import scrolling  # Import your main script
+
+scrolling.main()  # Replace with your actual main function name if needed
---
+++ scrolling.py
@@ -0,0 +1,50 @@
+from interstate75 import Interstate75, DISPLAY_INTERSTATE75_32X32
+import time
+
+i75 = Interstate75(display=Interstate75.DISPLAY_INTERSTATE75_32X32, panel_type=Interstate75.PANEL_FM6126A)
+graphics = i75.display
+
+width = i75.width
+height = i75.height
+
+RED = graphics.create_pen(255, 0, 0)
+PURPLE = graphics.create_pen(255, 0, 255)
+BLACK = graphics.create_pen(0, 0, 0)
+
+def text(content, color, left, down, fontsize):
+    graphics.set_pen(color)
+    graphics.text(content, left, down, False, fontsize)
+
+def clear_screen():
+    graphics.set_pen(BLACK)
+    graphics.clear()
+    i75.update()
+
+def scroll_marquee(text_content, color, fontsize, scroll_speed, y_position):
+    # Replace spaces with No-Break Space character
+    text_content = text_content.replace(' ', '\u00A0')
+
+    text_width = len(text_content) * fontsize * 6  # Approximate text width
+    x_position = width  # Start off the right edge of the display
+
+    while True:
+        clear_screen()
+        text(text_content, color, x_position, y_position, fontsize)
+        
+        x_position -= 1  # Move the text one pixel to the left
+
+        # If the text has scrolled completely off the left edge, reset its position
+        if x_position + text_width < 0:
+            x_position = width
+
+        i75.update()
+        time.sleep(scroll_speed)
+
+
+def main():
+    clear_screen()
+    scroll_marquee('HELLO WORLD HOW ARE YOU?', PURPLE, 2, 0.05, 15)
+
+
+if __name__ == "__main__":
+    main()
